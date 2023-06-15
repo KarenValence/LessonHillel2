2de972b8df7fc27a6da290cef1f708f7b4852723
KarenValence <karen_odessa@hotmail.com>
2023-06-15 21:01:57 +0200

    perezda4a1

---
+++ Src/perezda4a.java
@@ -0,0 +1,13 @@
+public class perezda4a {
+    public static void main(String[] args) {
+        int count = 0, value = 0;
+        while (count < 100) {
+            String s = String.valueOf(++value);
+            if ((s.contains("4") || s.contains("9"))) continue;
+            System.out.println(value);
+            count++;
+        }
+        System.out.println("TOTAL: " + count);
+    }
+    }
+
